b989c096472ae6c8105d5a09835bb470d88fe06e
Hongan Li <hongli@redhat.com>
2021-07-20 16:22:32 +0800

    Update aws-route53-cli.sh

--- aws-route53-cli.sh
+++ aws-route53-cli.sh
@@ -5,3 +5,5 @@ aws route53 list-resource-record-sets --hosted-zone-id ${HOSTED_ZONE_ID} --query
 ## find all hosted zones
 aws route53 list-hosted-zones | jq '.HostedZones[].Name'
 
+## find all Alias records for api.<doamin> 
+aws route53 list-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID | jq '.ResourceRecordSets[]|select(has("AliasTarget"))|select(.Name | startswith("api"))|.Name'
